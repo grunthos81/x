c6fa1ef8291267c9d700bd1fca7c8d6dbb7a4386
grunthos81 <47046437+grunthos81@users.noreply.github.com>
2019-02-09 01:31:25 +0000

    Add files via upload

---
+++ p3.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class p3
+    {
+        public static void Main(String[] args)
+        {
+            long n = 600851475143;
+            long p = 2; 
+            while (n > p)
+            {
+                if (n % p == 0)
+                {
+                    n = n / p;
+                }
+                else
+                {
+                    p += 1;
+                }
+            }
+            Console.WriteLine(n);
+        }
+    }
+}
