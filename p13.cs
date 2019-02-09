1bdc587b78552b55c44eb953e72b901fe40f032d
grunthos81 <47046437+grunthos81@users.noreply.github.com>
2019-02-09 23:35:51 +0000

    Add files via upload

---
+++ p13.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.IO;
+
+namespace ConsoleApp1
+{
+    class p13
+    {
+        public static void Main(String[] args)
+        {
+            using (var reader = new StreamReader("13.csv"))
+            {
+                long total = 0;
+                while (!reader.EndOfStream)
+                {
+                    
+                    var line = reader.ReadLine();
+                    total += long.Parse(line.Substring(0,11));
+                    
+                }
+                Console.WriteLine(total.ToString().Substring(0,10));
+            }
+        }
+    }
+}
