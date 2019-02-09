996adad8cce43bc4a499d4e8b6373401421fd583
grunthos81 <47046437+grunthos81@users.noreply.github.com>
2019-02-09 02:20:11 +0000

    Add files via upload

---
+++ p5.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class p5
+    {
+        public static void Main(String[] args)
+        {
+            Console.WriteLine(5 * 7 * 9 * 11 * 13 * 16 * 17 * 19);
+        }
+    }
+}
