bd2c8fdbc41311e938685ea41894213588484fad
grunthos81 <47046437+grunthos81@users.noreply.github.com>
2019-02-09 02:35:29 +0000

    Add files via upload

---
+++ p6.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class p6
+    {
+        public static void Main(String[] args)
+        {
+            //sum of squares can be found using Gauss' formula n(n+1)(2n+1)/6
+            //sum of naturals is just n(n+1)/2, a triangular number
+            long sumOfSquares = (100 * 101 * 201) / 6;
+            long justTheSum = (long)Math.Pow((100*101)/2,2);
+            Console.WriteLine(justTheSum-sumOfSquares);
+        }
+
+    }
+}
