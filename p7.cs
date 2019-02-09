301412d7afa5ca58f4e0da3a6095e63923033d7c
grunthos81 <47046437+grunthos81@users.noreply.github.com>
2019-02-09 14:12:24 +0000

    Add files via upload

---
+++ p7.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class p7
+    {
+        public static void Main(String[] args)
+        {
+            sieve s = new sieve();
+            bool[] nums = s.getSieve(200000);
+            int count = 0;
+            for (int i = 0; i < nums.Length; i++)
+            {
+                if (nums[i] == true) { count += 1; }
+                if (count == 10001)
+                {
+                    Console.WriteLine(i);
+                    break;
+
+                }
+            }
+
+        }
+    }
+}
---
+++ sieve.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class sieve
+    {
+
+        public Boolean[] getSieve(int size)
+        {
+            Boolean[] nums = new bool[size];
+            for(int i = 0; i < size; i++) { nums[i] = true; }
+            nums[0] = false;
+            nums[1] = false;
+            int p = 2;
+            while (p*p <= size)
+            {
+                if (nums[p] == true)
+                {
+                    for(int i = p*2; i < size; i+=p)
+                    {
+                        nums[i] = false;
+                    }
+                }
+                p++;
+            }
+            return nums;
+        }
+    }
+}
