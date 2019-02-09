e072d657af09cc3720c8c2ded22d947ab2d3d895
grunthos81 <47046437+grunthos81@users.noreply.github.com>
2019-02-09 01:17:46 +0000

    Add files via upload

---
+++ p1.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class p1
+    {
+        static void Main(string[] args)
+        {
+            int total = 0; 
+            for(int i = 1; i < 1000; i++)
+            {
+                if ((i % 3 == 0) ||(i%5==0))
+                {
+                    total += i;
+                }
+            }
+            Console.WriteLine(total);
+        }
+    }
+}
