0758d6815972fb2360bae87ce67ed83dca9c3be8
grunthos81 <47046437+grunthos81@users.noreply.github.com>
2019-02-09 01:23:57 +0000

    Add files via upload

---
+++ p2.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class p2
+    {
+        public static void Main(String[] args)
+        {
+            int total = 2;
+            int a = 1;
+            int b = 2; 
+            while(b < 4000000)
+            {
+                int c = a + b;
+                if (c%2 == 0) { total += c; }
+                a = b;
+                b = c; 
+            }
+            Console.WriteLine(total);
+        }
+                
+    }
+}
