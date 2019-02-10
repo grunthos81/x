ec4a3252fb767c28dc4396e4eff756cb3428135b
grunthos81 <47046437+grunthos81@users.noreply.github.com>
2019-02-10 17:37:54 +0000

    Add files via upload

---
+++ p16.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Numerics;
+
+namespace ConsoleApp1
+{
+    class p16
+    {
+        public static BigInteger numDigits(BigInteger a)
+        {
+            BigInteger total = new BigInteger(0);
+            while (a > 0)
+            {
+                total += a % 10;
+                a /= 10;
+            }
+            return total;
+        }
+        public static void Main(String[] args)
+        {
+            BigInteger num = new BigInteger(Math.Pow(2, 1000));
+            Console.WriteLine(numDigits(num));
+        }
+    }
+}
