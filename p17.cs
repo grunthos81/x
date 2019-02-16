b345a85064789fc25854b0664fb0a03638355235
grunthos81 <47046437+grunthos81@users.noreply.github.com>
2019-02-16 00:12:59 +0000

    Add files via upload

---
+++ p17.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp1
+{
+    class p17alt
+    {
+        public static void Main(String[] args)
+        {
+            string[] hundreds = {"","onehundredand", "twohundredand", "threehundredand", "fourhundredand",
+            "fivehundredand", "sixhundredand", "sevenhundredand", "eighthundredand", "ninehundredand"};
+            string[] tens = {"", "", "twenty", "thirty", "forty", "fifty",
+                "sixty", "seventy", "eighty", "ninety"};
+            string[] units = {"", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine",
+            "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen",
+            "eighteen", "nineteen"};
+            int total = 11;
+            for(int i = 1; i < 1000; i++)
+            {
+                if (i % 100 == 0)
+                {
+                    total += hundreds[i / 100].Length - 3;
+                    continue;
+                }
+                if (i % 100 < 20)
+                {
+                    total += (hundreds[i / 100] + units[i % 100]).Length;
+                }
+                else
+                {
+                    total += (hundreds[i / 100] + tens[(i % 100)/10] + units[i % 10]).Length;
+                }
+
+            }
+            Console.WriteLine(total);
+        }
+    }
+}
